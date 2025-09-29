229b479f95ff3d3ba8bc6785bf838762e857709c
Pascal <pascaldenzel7@gmail.com>
2025-09-30 00:16:32 +0300

    Updated frontend URL

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 export async function getTasks() {
   console.log('getTasks called, BASE_URL:', BASE_URL);
